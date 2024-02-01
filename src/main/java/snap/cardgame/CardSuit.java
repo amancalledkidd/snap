2bc632325fcec5932bca39f844475377c20ec3fd
Kumani K <kumanikidd@gmail.com>
2024-02-01 13:07:34 +0000

    Suit enum created

---
+++ src/main/java/snap/cardgame/CardSuit.java
@@ -0,0 +1,14 @@
+package snap.cardgame;
+
+public enum CardSuit {
+    Hearts("U+2665"),
+    Diamonds("U+2666"),
+    Spades("U+2660"),
+    Clubs("U+2663");
+
+    private final String unicodeImage;
+
+    CardSuit(String unicodeImage) {
+        this.unicodeImage = unicodeImage;
+    }
+}
